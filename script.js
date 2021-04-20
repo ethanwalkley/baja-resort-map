5069c1a04eeaa99e11fb46ed7f0acdf20c6f23ed
ethanwalkley <43659050+ethanwalkley@users.noreply.github.com>
2021-04-20 13:11:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,29 @@
-var tooltip = document.querySelector(".map-tooltip");
-var colors = [
-  "#1f77b4",
-  "#ff7f0e",
-  "#2ca02c",
-  "#d62728",
-  "#9467bd",
-  "#8c564b",
-  "#e377c2",
-  "#7f7f7f",
-  "#bcbd22",
-  "#17becf",
-];
-var contents = {};
-for (var i = 1; i <= 10; i++) {
-  contents["tooltip" + i] =
-    'tooltip <b style="color:' + colors[i] + '">' + i + "</b>";
-}
-
-// iterate throw all `path` tags
-[].forEach.call(document.querySelectorAll("path.HI-map"), function (item) {
-  // attach click event, you can read the URL from a attribute for example.
-  item.addEventListener("click", function () {
-    window.open("http://google.co.il");
-  });
-
-  // attach mouseenter event
-  item.addEventListener("mouseenter", function () {
-    tooltip.innerHTML = contents[item.getAttribute("data-tooltip")];
-    tooltip.style.display = "block";
-  });
-
-  item.addEventListener("mousemove", function (e) {
-    tooltip.style.top = e.clientY + "px";
-    tooltip.style.left = e.clientX + "px";
-  });
-
-  // when mouse leave hide the tooltip
-  item.addEventListener("mouseleave", function () {
-    tooltip.style.display = "none";
-  });
+$(".st0, .st1, .st2, .st3, .st4, .st5").hover(function (e) {
+  $("#info-box").css("opacity", "100%");
+  $("#info-box").html($(this).data("info"));
 });
+
+$(".st26").hover(function (e) {
+  $("#info-box").css("opacity", "100%");
+  $("#info-box").html($(this).data("info"));
+});
+
+$(".st0, .st1, .st2, .st3, .st4, .st5").mouseleave(function (e) {
+  $("#info-box").css("opacity", "0%");
+});
+
+$(document)
+  .mousemove(function (e) {
+    $("#info-box").css("top", e.pageY - $("#info-box").height() - 30);
+    $("#info-box").css("left", e.pageX - $("#info-box").width() / 2);
+  })
+  .mouseover();
+
+var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+if (ios) {
+  $("a").on("click touchend", function () {
+    var link = $(this).attr("href");
+    window.open(link, "_blank");
+    return false;
+  });
+}
